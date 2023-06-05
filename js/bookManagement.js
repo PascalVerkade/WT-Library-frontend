1946d6341a3e8b4fd05041d365e99e31caf01efa
Marentheh <marenthehopma@gmail.com>
2023-06-05 10:03:36 +0200

    book management page largely functional

--- js/bookManagement.js
+++ js/bookManagement.js
@@ -1,15 +1,21 @@
+var selectedBook = null
+
+
 function selectBook(bookId) {
     fetch(`http://localhost:8080/book/${bookId}`)
         .then(res => res.json())
         .then(data => {
 
-            document.getElementById('UpdateHeader').innerHTML = `Update ${data.title}`
+            document.getElementById('UpdateHeader').innerHTML = `Update ${data.title}`;
+            selectedBook = data;
 
         })
     console.log(bookId)
+    
     document.getElementById('addCopy').disabled = false;
+    document.getElementById('updateBook').disabled = false;
+    document.getElementById('archiveBook').disabled = false;
 }
-
   
 
 function loadAllBooks() {
@@ -30,7 +36,7 @@ function loadAllBooks() {
                         <td>${book.photo}</td>
                         <td>${book.title}</td>
                         <td>${book.writer}</td>
-                        <td><button name="select" class="btn-select" id="${book.id}" onclick="selectBook(${book.id})">Selecteren</button></td>
+                        <td><button name="select" class="btn-select" bookId="${book.id}" onclick="selectBook(${book.id})">Selecteren</button></td>
                     </tr>
                 `
             });
@@ -43,3 +49,15 @@ function loadAllBooks() {
 }
 
 loadAllBooks();
+
+function copyThisBook() {
+    document.getElementById('copyAddedHeader').innerHTML = `A copy of ${selectedBook.title} was added`;
+}
+
+function updateThisBook() {
+    document.getElementById('bookUpdatedHeader').innerHTML = `${selectedBook.title} was updated`;
+}
+
+function archiveThisBook() {
+    document.getElementById('bookArchivedHeader').innerHTML = `${selectedBook.title} was archived`;
+}
