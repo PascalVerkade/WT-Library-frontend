884da13be462872933b44756ac1a159b6db033fc
Marentheh <marenthehopma@gmail.com>
2023-06-07 14:43:17 +0200

    updating books page added

--- js/updateBook.js
+++ js/updateBook.js
@@ -1,11 +1,21 @@
 var selectedBook = null
-var copyCount = null
-var selectedCopy = null
 
 function setHeaders() {
     console.log('setting headers')
     document.getElementById('bookTitle').innerHTML = `${selectedBook.title}`;
     document.getElementById('bookDetails').innerHTML = `Written by: ${selectedBook.writer}<br>ISBN: ${selectedBook.isbn}`;
+}
+
+function fillTable() {
+    document.getElementById('title').value = `${selectedBook.title}`;
+    document.getElementById('writer').value = `${selectedBook.writer}`;
+    document.getElementById('isbn').value = `${selectedBook.isbn}`;
+    document.getElementById('photo').value = `${selectedBook.photo}`;
+    document.getElementById('stock').value = `${selectedBook.stock}`;
+    if (selectedBook.available){
+        document.getElementById('available').checked = true;
+    }
+    else {document.getElementById('available').checked = false;}
 }
 
 function getBook() {
@@ -20,7 +30,29 @@ function getBook() {
             console.log('book: '+bookId)
             selectedBook = data;
             setHeaders();
+            fillTable();
         })
+}
+
+function updateThisBook() {
+    selectedBook.title = document.getElementById('title').value
+    selectedBook.writer = document.getElementById('writer').value
+    selectedBook.isbn = document.getElementById('isbn').value
+    selectedBook.photo = document.getElementById('photo').value
+    fetch(`http://localhost:8080/book/update/${selectedBook.id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(selectedBook)
+    })
+    .then(data => {
+        alert('Boek is geupdated!')
+    })
+    .catch(error => {
+        console.log(error);
+        alert('Er is iets fouts gegaan');
+    })
 }
 
 
