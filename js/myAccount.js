bacf68d7d58f142b2157d0529f804320cda87c29
Eddertje <eddertjeje@gmail.com>
2023-06-19 15:22:23 +0200

    fixed spaggetti code YAAAAAAY

--- js/myAccount.js
+++ js/myAccount.js
@@ -2,10 +2,9 @@ window.onload = getUser();
 
 function getUser() {
     let data = {
-        "email": "test",
-        "password": "test"
+        "email": localStorage.getItem("email")
     }
-    fetch("http://localhost:8080/employee/login", {
+    fetch("http://localhost:8080/employee/get", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -21,58 +20,32 @@ function getUser() {
             document.getElementById("passwordButton").onclick = () => {
                 
                 if(document.getElementById("password").value.length != 0) {
-                    console.log("hi")
                     if(confirm("Are you sure you want to change your password?")) {
-                       changePassword(data.employeeId); 
+                       changeValues(data.employeeId); 
                     }
+                } else {
+                    changeValues(data.employeeId);
                 }
-                changeFirstName(data.employeeId);
-                changeLastName(data.employeeId);
             }
         }).catch(error => console.error(error));
 }
 
-function changeFirstName(id) {
+function changeValues(id) {
     let data = {
         "firstName": document.getElementById("firstName").value,
-        "employeeId": id
-    }
-    fetch("http://localhost:8080/employee/changeFirstName", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': localStorage.getItem("token")
-        },
-        body: JSON.stringify(data)
-    }).then(() => getUser())
-}
-
-function changeLastName(id) {
-    let data = {
         "lastName": document.getElementById("lastName").value,
-        "employeeId": id
-    }
-    fetch("http://localhost:8080/employee/changeLastName", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': localStorage.getItem("token")
-        },
-        body: JSON.stringify(data)
-    }).then(() => getUser())
-}
-
-function changePassword(id) {
-    let data = {
         "password": document.getElementById("password").value,
         "employeeId": id
     }
-    fetch("http://localhost:8080/employee/changePassword", {
+    if(document.getElementById("password").value.length == 0) {
+        data["password"] = null;
+    }
+    fetch("http://localhost:8080/employee/changeValues", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': localStorage.getItem("token")
         },
         body: JSON.stringify(data)
-    }).then(() => alert("Password changed"))
+    }).then(() => getUser())
 }
